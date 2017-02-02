3f0955a2eff4f29b6be78f2ff691d3c8226c9371
Aayush aryan <aayush.aryan91@gmail.com>
2017-02-02 15:22:57 +0530

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -11,8 +11,8 @@ public class AreaOfCircle extends ConsoleProgram
 	{
 		println("This program calculates area of a circle taking radius value from the user.");
 		double radius = readDouble("Enter the radius: ");
-		double area = (pi * radius * radius);
+		double area = (PI * radius * radius);
 		println("The area of the circle is " + area + " units.");
 	}
-		private static final double pi = 3.142857142;
+		private static final double PI = 3.142857142;
 }
